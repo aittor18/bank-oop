e2392d408d22fc636d68bbe0632fc6ac43071603
aittor18 <rodriguezaitor678@gmail.com>
2024-11-29 12:36:04 +0100

    BankReader terminado

---
+++ src/org/ies/bank/components/BankReader.java
@@ -0,0 +1,31 @@
+package org.ies.bank.components;
+
+import org.ies.bank.model.Account;
+import org.ies.bank.model.Bank;
+
+import java.util.Scanner;
+
+public class BankReader {
+    private final Scanner scanner;
+    private final AccountReader accountReader;
+
+    public BankReader(Scanner scanner, AccountReader accountReader) {
+        this.scanner = scanner;
+        this.accountReader = accountReader;
+    }
+
+    public Bank read() {
+        System.out.println("Introduce los datos del banco");
+        System.out.print("Nombre del banco: ");
+        String name = scanner.nextLine();
+        System.out.print("¿Cuántas cuentas hay en el banco?: ");
+        int size = scanner.nextInt();
+        scanner.nextLine();
+
+        Account[] accounts = new Account[size];
+        for (int i = 0; i < size; i++) {
+            accounts[i] = accountReader.read();
+        }
+        return new Bank(name, accounts);
+    }
+}
