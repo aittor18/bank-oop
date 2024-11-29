d9096d5fbbbb897a5285c7f921806368cc0e0ad7
aittor18 <rodriguezaitor678@gmail.com>
2024-11-29 12:28:01 +0100

    AccountReader terminado

--- src/org/ies/bank/components/AccountReader.java
+++ src/org/ies/bank/components/AccountReader.java
@@ -1,14 +1,17 @@
 package org.ies.bank.components;
 
 import org.ies.bank.model.Account;
+import org.ies.bank.model.Customer;
 
 import java.util.Scanner;
 
 public class AccountReader {
     private final Scanner scanner;
+    private final CustomerReader customerReader;
 
-    public AccountReader(Scanner scanner) {
+    public AccountReader(Scanner scanner, CustomerReader customerReader) {
         this.scanner = scanner;
+        this.customerReader = customerReader;
     }
 
     public Account read() {
@@ -18,6 +21,17 @@ public class AccountReader {
         System.out.print("Saldo: ");
         double balance = scanner.nextDouble();
         System.out.print("Ahora, ¿cuántos clientes hay?: ");
+        int size = scanner.nextInt();
+        scanner.nextLine();
 
+        Customer[] customers = new Customer[size];
+        for (int i = 0; i < size; i++) {
+            customers[i] = customerReader.read();
+        }
+        return new Account(
+                iban,
+                balance,
+                customers
+        );
     }
 }
