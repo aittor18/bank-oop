c7a2810e1cd922105c23f8158fcaf9555449212f
aittor18 <rodriguezaitor678@gmail.com>
2024-12-05 10:46:39 +0100

    Solucionando cositas y recuperando mi cordura :)

--- src/org/ies/bank/Main.java
+++ src/org/ies/bank/Main.java
@@ -1,6 +1,9 @@
 package org.ies.bank;
 
+import org.ies.bank.components.AccountReader;
 import org.ies.bank.components.BankApp;
+import org.ies.bank.components.BankReader;
+import org.ies.bank.components.CustomerReader;
 import org.ies.bank.model.Bank;
 
 import java.util.Scanner;
@@ -8,5 +11,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CustomerReader customerReader = new CustomerReader(scanner);
+        AccountReader accountReader = new AccountReader(scanner, customerReader);
+        BankReader bankReader = new BankReader(scanner, accountReader);
+
+        bankApp.run();
     }
 }
--- src/org/ies/bank/components/BankApp.java
+++ src/org/ies/bank/components/BankApp.java
@@ -1,25 +1,50 @@
 package org.ies.bank.components;
 
+import org.ies.bank.model.Account;
 import org.ies.bank.model.Bank;
 
 import java.util.Scanner;
 
 public class BankApp {
-    private final BankReader bankReader;
+    private final Bank bank;
     private final Scanner scanner;
 
-    public BankApp(BankReader bankReader, Scanner scanner) {
-        this.bankReader = bankReader;
+    public BankApp(Bank bank, Scanner scanner) {
+        this.bank = bank;
         this.scanner = scanner;
     }
 
     public void run() {
-        Scanner scanner = new Scanner(System.in);
-        CustomerReader customerReader = new CustomerReader(scanner);
-        AccountReader accountReader = new AccountReader(scanner, customerReader);
-        BankReader bankReader = new BankReader(scanner, accountReader);
-        BankApp bankApp = new BankApp(bankReader, scanner);
+        int option;
+        do {
+            System.out.println("Elige una opción");
+            System.out.println("1. Mostrar cuentas");
+            System.out.println("2. Mostrar datos de cuenta");
+            System.out.println("3. Mostrar cuentas de un cliente");
+            System.out.println("4. Ingresar dinero");
+            System.out.println("5. Sacar dinero");
+            System.out.println("6. Salir");
+            System.out.print("Introduce la opción que quieras: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
 
-        bankApp.run();
+            switch (option) {
+                case 1:
+                    bank.showAccounts();
+                    break;
+                case 2:
+                    System.out.println("Introduce el IBAN: ");
+                    String iban = scanner.nextLine();
+                    Account account = bank.findAccount(iban);
+                    if (!account.getIban().equals(iban)) {
+                        account.showInfo();
+                    } else {
+                        System.out.println("Cuenta no encontrada");
+                    }
+                    break;
+                case 3:
+
+            }
+        } while (option != 6);
     }
 }
